9525caf4e9456e024a90335c6ac3f29024976651
Sagar Saby <--global>
2023-04-14 13:31:15 -0700

    Separated data from templates and placed them into controllers

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,6 +1,14 @@
 /* About Page */
 const about = (req, res) => {
-    res.render('about', { title: 'About' });
+    res.render('about', { 
+        title: 'About',
+        content: `Locater was created for practice as I followed the book "Getting MEAN with Mongo, Express, Angular, and Node" 
+        \n This is filler text 
+        Locater was created for practice as I followed the book "Getting MEAN with Mongo, Express, Angular, and Node" Locater was created for practice as I followed the book "Getting MEAN with Mongo, Express, Angular, and Node"
+        Locater was created for practice as I followed the book "Getting MEAN with Mongo, Express, Angular, and Node"
+        Locater was created for practice as I followed the book "Getting MEAN with Mongo, Express, Angular, and Node"
+        Locater was created for practice as I followed the book "Getting MEAN with Mongo, Express, Angular, and Node"`
+    });
 };
 
 module.exports = {
