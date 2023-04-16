1f9370ffbc749110ed3bb6884abd5b7ddb854c56
s-ssaby <sabysagar@gmail.com>
2023-04-16 14:32:13 -0700

    Set up database in db.js. Schema not defined yet

---
+++ app_server/models/db.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const dbURI = 'mongodb://localhost/Locater'
+mongoose.connect(dbURI, {useNewUrlParser: true});
+
+mongoose.connection.on('connected', () => {
+    console.log(`Mongoose connected to ${dbURI}`);
+});
+
+mongoose.connection.on('error', err => {
+    console.log('Mongoose connection error:', err);
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose disconnected');
+});
+
+const gracefulShutdown = (msg, callback) => {
+    mongoose.connection.close( () => {
+        console.log(`Mongoose disconnected through ${msg}`);
+        callback();
+    });
+};
+
+process.once('SIGUSR2', () => {
+    gracefulShutdown('nodemon restart', () => {
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+
+process.once('SIGINT', () => {
+    gracefulShutdown('app termination', () => {
+        process.exit(0);
+    });
+});
+
+process.once('SIGTERM', () => {
+    gracefulShutdown('Heroku app shutdown', () => {
+        process.exit(0);
+    });
+});
+
+require('./locations');
