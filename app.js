8eed68ad3d5f3948953a0176573085b103236a9b
s-ssaby <sabysagar@gmail.com>
2023-04-16 21:40:44 -0700

    Removed user routes and started work on API routes in app_api folder

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const logger = require('morgan');
 require('./app_server/models/db');
 
 const indexRouter = require('./app_server/routes/index');
-const usersRouter = require('./app_server/routes/users');
+
 
 const app = express();
 
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
