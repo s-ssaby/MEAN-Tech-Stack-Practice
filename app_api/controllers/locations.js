d2e1c5385d63d6dbaac703b3965aed62bf87b562
Sagar Saby <--global>
2023-04-18 19:27:18 -0700

    Started work on controllers for API in app_api/controllers

---
+++ app_api/controllers/locations.js
@@ -0,0 +1,13 @@
+const locationsCreate = (req, res) => {};
+const locationsReadOne = (req, res) => {};
+const locationsUpdateOne = (req, res) => {};
+const locationsDeleteOne = (req, res) => {};
+const locationsListByDistance = (req, res) => {};
+
+module.exports = {
+    locationsCreate,
+    locationsReadOne,
+    locationsUpdateOne,
+    locationsDeleteOne,
+    locationsListByDistance
+};
---
+++ app_api/controllers/reviews.js
@@ -0,0 +1,11 @@
+const reviewsCreate = (req, res) => {};
+const reviewsReadOne = (req, res) => {};
+const reviewsUpdateOne = (req, res) => {};
+const reviewsDeleteOne = (req, res) => {};
+
+module.exports = {
+    reviewsCreate,
+    reviewsDeleteOne,
+    reviewsReadOne,
+    reviewsUpdateOne
+};
