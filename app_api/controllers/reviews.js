605f7e1d6a7d1fce2d12c42523f08883b81c2595
s-ssaby <sabysagar@gmail.com>
2023-04-18 22:07:19 -0700

    Added reviewsReadOne API function

--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -7,9 +7,44 @@ const reviewsCreate = (req, res) => {
         .json({"status": "success"});
 };
 const reviewsReadOne = (req, res) => {
-    res
-        .status(200)
-        .json({"status": "success"});
+    LocationModel
+        .findById(req.params.locationid)
+        .select('name reviews')
+        .exec((err, location) => {
+            if (!location) {
+                return res
+                    .status(404)
+                    .json({"message": "Location not found"});
+            } else if (err) {
+                return res
+                    .status(400)
+                    .json(err);
+            }
+            // if location has reviews
+            if (location.reviews && location.reviews.length > 0) {
+                const review = location.reviews.id(req.params.reviewid);
+                if (!review) {
+                    return res
+                        .status(400)
+                        .json( {"message": "Review not found"});
+                } else {
+                    response = {
+                        location: {
+                            name: location.name,
+                            id: req.params.locationid
+                        },
+                        review
+                    };
+                    return res
+                        .status(200)
+                        .json(response);
+                }
+            } else {
+                return res
+                    .status(404)
+                    .json({"message": "No reviews found"});
+            }
+        });
 };
 const reviewsUpdateOne = (req, res) => {
     res
